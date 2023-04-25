2087924f999b7ebedb9aceb924b4f4446f91f05d
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-25 10:06:09 -0700

    wip: comment + post schema

--- models/community.js
+++ models/community.js
@@ -1,10 +1,46 @@
 //require mongoose
 const mongoose = require("mongoose");
-//set up the schema for the community model
 const Schema = mongoose.Schema;
 
+// likes = array of Users
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  users: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ]
+});
+
+// likes = array of Users
+const postSchema = new Schema({
+  subject: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  comments: [commentSchema],
+  users: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ]
+});
+
 const communitySchema = new Schema({
-  name: { type: String }
+  name: {
+    type: String,
+    required: true
+  },
+  posts: [postSchema]
 });
 
 module.exports = mongoose.model("Community", communitySchema);

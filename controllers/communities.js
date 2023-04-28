e28462fa3e27f21c8c33e930b751a833d3278a6d
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-27 22:04:28 -0700

    style posts

--- controllers/communities.js
+++ controllers/communities.js
@@ -3,7 +3,6 @@ const Community = require("../models/community");
 async function index(req, res) {
   try {
     const community = await Community.findOne({ community: req.params.name });
-    //todo: create view template
     res.render("communities/index", {
       title: `${req.params.name} community`,
       posts: community.posts,
